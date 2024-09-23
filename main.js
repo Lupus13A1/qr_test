b0618be2f326d7370da8b068c686a7cca04b38c5
Lupus13A1 <symphoniga@gmail.com>
2024-09-23 20:57:51 +0700

    pass

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // ตัวแปรสำหรับเก็บสถานะการปิด QR Code ของแต่ละโต๊ะ
 let qrCodeStatus = {
-  table1: true,
+  table1: false,
   table2: true,
   table3: true,
 };
 
-// สร้าง QR Code สำหรับโต๊ะที่ต้องการ
+// สร้าง QR Code สำหรับโต๊ะ
 function generateQRCode(tableId, url) {
   if (qrCodeStatus[tableId]) {
     new QRCode(document.getElementById(`qrcode-${tableId}`), url);
@@ -15,20 +15,20 @@ function generateQRCode(tableId, url) {
 // ฟังก์ชันปิด QR Code
 function closeQRCode(tableId) {
   qrCodeStatus[tableId] = false;
-  document.getElementById(`qrcode-${tableId}`).innerHTML = ""; // ลบ QR Code ออกจากหน้าจอ
+  document.getElementById(`qrcode-${tableId}`).innerHTML = ""; // ลบ QR Code
   alert(`QR Code สำหรับโต๊ะ ${tableId} ถูกปิดแล้ว`);
 }
 
-// ฟังก์ชันตรวจสอบสถานะ QR Code เมื่อสแกน
+// ฟังก์ชันเช็ค QR Code เมื่อสแกน
 function checkQRCodeStatus(tableId) {
   if (!qrCodeStatus[tableId]) {
     window.location.href = "https://qrtest01.netlify.app/404"; // เปลี่ยนไปหน้า 404
   } else {
-    window.location.href = `https://qrtest01.netlify.app/menu?table=${tableId}`; // เปลี่ยนไปหน้าเมนูตามโต๊ะ
+    window.location.href = `https://qrtest01.netlify.app/menu?table=${tableId}`; // เปลี่ยนตามโต๊ะ
   }
 }
 
-// เมื่อกดปุ่มแสดง QR Code ให้สร้าง QR Code สำหรับโต๊ะนั้นๆ
+// สร้าง qr สำหรับโต๊ะ
 document
   .getElementById("generate-table1")
   .addEventListener("click", function () {
@@ -47,7 +47,7 @@ document
     generateQRCode("table3", "https://qrtest01.netlify.app/menu?table=3");
   });
 
-// เพิ่มการทำงานให้กับปุ่มปิด QR Code
+// ปุ่มปิด QR Code
 document.querySelectorAll("button.closeQR").forEach(function (button) {
   button.addEventListener("click", function () {
     const tableId = this.getAttribute("data-table");
